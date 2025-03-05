f3fd6cb8ac929c89410a9c7d216666bd7ec105c7
Jonathan Silveira <jonathan.silveira@picpay.com>
2025-03-05 17:31:17 -0300

    Implementar função para saber se client está fechado e tratamento de exceção para abertura/fechamento de conexão

--- src/data/opensearch/opensearch_client_wrapper.py
+++ src/data/opensearch/opensearch_client_wrapper.py
@@ -16,32 +16,41 @@ class OpenSearchClientWrapper:
         self.client = None
 
     def open(self) -> None:
-        self.client = OpenSearch(
-            hosts = self.connection_config.hosts,
-            http_auth = self.connection_config.auth, 
-            http_compress = self.connection_config.http_compress, # enables gzip compression for request bodies
-            use_ssl = self.connection_config.use_ssl,
-            verify_certs = self.connection_config.verify_certs,
-            ssl_assert_hostname = self.connection_config.ssl_assert_hostname,
-            ssl_show_warn = self.connection_config.ssl_show_warn
-        )
+        try:
+            self.client = OpenSearch(
+                hosts = self.connection_config.hosts,
+                http_auth = self.connection_config.auth, 
+                http_compress = self.connection_config.http_compress, # enables gzip compression for request bodies
+                use_ssl = self.connection_config.use_ssl,
+                verify_certs = self.connection_config.verify_certs,
+                ssl_assert_hostname = self.connection_config.ssl_assert_hostname,
+                ssl_show_warn = self.connection_config.ssl_show_warn
+            )
+        except Exception as e:
+            self.client = None
+            raise Exception(f"Error while trying to open OpenSearch client: {e}")
 
-    def close(self) -> None:
-        if not self.client:
+    def close_quietly(self) -> None:
+        if self.isclosed():
             return
-        self.client.close()
-        self.client = None
+        try:
+            self.client.close()
+        finally:
+            self.client = None
 
     def reopen(self) -> None:
-        self.close()
+        self.close_quietly()
         self.open()
+
+    def isclosed(self) -> bool:
+        return self.client is None
     
     def execute_query(
         self, 
         query: dict,
         index: str
     ) -> list:
-        if not self.client:
+        if self.isclosed():
             raise Exception(f"OpenSearch client not initialized yet!")
         response = self.client.search(
             body = query,
