1aa3735cb35bb6874baabbe298b2742cc9ba870a
Jonathan Silveira <jonathan.silveira@picpay.com>
2025-03-07 15:03:44 -0300

    Pegar primeiro parâmetro da execução para carregar o env

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,3 +1,9 @@
-from dotenv import load_dotenv
+import sys
 
-load_dotenv()
+from model.config_env import ConfigEnv
+from config.settings import Settings
+
+config_env_param = sys.argv[1] if len(sys.argv) > 1 else ConfigEnv.QA.value
+config_env = ConfigEnv.from_value(config_env_param)
+
+settings = Settings(config_env=config_env)
